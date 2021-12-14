b769c1c4acebe5f2758d19d8018af16055f9d1a4
RAJEEV ANAND <rajeevanand840@gmail.com>
2021-12-14 16:50:21 +0530

    Update K-Mean.py

--- K-Mean.py
+++ K-Mean.py
@@ -1,1 +1,55 @@
+# Objectives¶
+# After completing this lab you will be able to:
+
+# Use scikit-learn's K-Means Clustering to cluster data
+
+
+# Introduction
+# There are many models for clustering out there. In this notebook, we will be presenting the model that is considered one of the simplest models amongst them. Despite its simplicity, the K-means is vastly used for clustering in many data science applications, it is especially useful if you need to quickly discover insights from unlabeled data. In this notebook, you will learn how to use k-Means for customer segmentation.
+
+# Some real-world applications of k-means:
+
+# Customer segmentation
+# Understand what the visitors of a website are trying to accomplish
+# Pattern recognition
+# Machine learning
+# Data compression
+# In this notebook we practice k-means clustering with 2 examples:
+
+# k-means on a random generated dataset
+# Using k-means for customer segmentation
+
+# Import libraries
+# Let's first import the required libraries. Also run %matplotlib inline since we will be plotting in this section.
+import random 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.datasets.samples_generator import make_blobs 
+%matplotlib inline
+
+# k-Means on a randomly generated dataset¶
+# Let's create our own dataset for this lab!
+
+# First we need to set a random seed. Use numpy's random.seed() function, where the seed will be set to 0
+np.random.seed(0)
+
+Next we will be making <i> random clusters </i> of points by using the <b> make_blobs </b> class. The <b> make_blobs </b> class can take in many inputs, but we will be using these specific ones. <br> <br> <b> <u> Input </u> </b>
+
+<ul>
+    <li> <b>n_samples</b>: The total number of points equally divided among clusters. </li>
+    <ul> <li> Value will be: 5000 </li> </ul>
+    <li> <b>centers</b>: The number of centers to generate, or the fixed center locations. </li>
+    <ul> <li> Value will be: [[4, 4], [-2, -1], [2, -3],[1,1]] </li> </ul>
+    <li> <b>cluster_std</b>: The standard deviation of the clusters. </li>
+    <ul> <li> Value will be: 0.9 </li> </ul>
+</ul>
+<br>
+<b> <u> Output </u> </b>
+<ul>
+    <li> <b>X</b>: Array of shape [n_samples, n_features]. (Feature Matrix)</li>
+    <ul> <li> The generated samples. </li> </ul> 
+    <li> <b>y</b>: Array of shape [n_samples]. (Response Vector)</li>
+    <ul> <li> The integer labels for cluster membership of each sample. </li> </ul>
+</ul>
 
